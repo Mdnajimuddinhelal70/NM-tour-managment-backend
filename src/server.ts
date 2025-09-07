1f8790cf2234f008a493d2aa64fdc0ec767a5513
unknown <najimuddinhelalal670@gmail.com>
2025-09-07 10:55:08 +0600

    setup completed

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,79 @@
+/* eslint-disable no-console */
 import { Server } from "http";
 import mongoose from "mongoose";
 import app from "./app";
+import { envVars } from "./app/config/env";
 
 let server: Server;
 
 const startServer = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://najimuddinhelal96_db_user:HsJyKMv65XXhucC1@cluster0.mzgzbpf.mongodb.net/nm-tour-management?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    await mongoose.connect(envVars.DB_URL);
 
     console.log("Connected to DB!!");
 
-    server = app.listen(5000, () => {
-      console.log("Server is listening on port 5000");
+    server = app.listen(envVars.PORT, () => {
+      console.log(`Server is listening on port ${envVars.PORT}`);
     });
   } catch (error) {
     console.log(error);
   }
 };
 startServer();
+
+// Unhandle rejection error
+process.on("unhandledRejection", (err) => {
+  console.log("Unhandle Rejection detected.. server shutting down...", err);
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+// uncaught rejection error
+process.on("SIGTERM", () => {
+  console.log("SIGTERM signal recieved shutting down...");
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+process.on("SIGINT", () => {
+  console.log("SIGINT signal recieved shutting down...");
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught Exception detected.. server shutting down...", err);
+
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+// Unhandle rejection error
+// Promise.reject(new Error("I forgot to catch this error"));
+
+// uncaught rejection error
+// throw new Error("I forgot to handle this local error");
+
+/**
+ * Unhandle rejection error
+ * uncaught rejection error
+ * signal termination sigtern
+ * */
