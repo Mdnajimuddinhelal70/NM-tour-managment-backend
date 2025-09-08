236c0bbff4d5c9766081ba1247cb19f47e3ba172
unknown <najimuddinhelalal670@gmail.com>
2025-09-08 22:12:36 +0600

    added error helpers and route not found

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import { globalEerrorHandler } from "./app/middlewares/globalerrorHandle";
 
 import cors from "cors";
 import express, { type Request, type Response } from "express";
+import httpStatus from "http-status-codes";
 import { router } from "./app/routes";
 const app = express();
 
@@ -15,4 +16,10 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use(globalEerrorHandler);
+app.use((req: Request, res: Response) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: "Route Not Found",
+  });
+});
 export default app;
