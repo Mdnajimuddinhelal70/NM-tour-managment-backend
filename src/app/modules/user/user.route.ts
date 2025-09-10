d2a50195fa97c1f10c2e6eaa4cc444f896456afd
unknown <najimuddinhelalal670@gmail.com>
2025-09-10 21:02:30 +0430

    created validateRequest

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import { validateRequest } from "../../middlewares/validateRequest";
 import { UserController } from "./user.controller";
+import { createUserZodSchema } from "./user.validation";
 
 const route = Router();
 
-route.post("/register", UserController.createUser);
+route.post(
+  "/register",
+  validateRequest(createUserZodSchema),
+  UserController.createUser
+);
 route.get("/all-users", UserController.getAllUsers);
 
 export const UserRoutes = route;
