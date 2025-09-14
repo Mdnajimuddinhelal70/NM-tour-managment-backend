427a3184360af8092f63d7861221ddb5081c706b
unknown <najimuddinhelalal670@gmail.com>
2025-09-14 21:25:50 +0430

    updaed register api

--- src/app/errorHelpers/AppError.ts
+++ src/app/errorHelpers/AppError.ts
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/no-inferrable-types */
 class AppError extends Error {
   public statusCode: number;
 
-  constructor(statusCode: number, message: string, stack: "") {
+  constructor(statusCode: number, message: string, stack: string = "") {
     super(message);
     this.statusCode = statusCode;
 
--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -8,7 +8,7 @@ export enum Role {
 }
 
 export interface IAuthProvider {
-  provider: string; //"google", "credential"
+  provider: "google" | "credentials";
   providerId: string;
 }
 
