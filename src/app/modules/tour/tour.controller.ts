68a9c9198fc5f5950bc2d40607ff44fda8b7e240
unknown <najimuddinhelalal670@gmail.com>
2025-10-29 11:23:24 +0600

    added updateTourType in controller function

--- src/app/modules/tour/tour.controller.ts
+++ src/app/modules/tour/tour.controller.ts
@@ -87,6 +87,18 @@ const createTourType = catchAsync(async (req: Request, res: Response) => {
     data: result,
   });
 });
+const updateTourType = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  if (!id) throw new AppError(400, "Tour ID is required");
+  const { name } = req.body;
+  const result = await TourService.updateTourType(id, name);
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Tour type updated successfully",
+    data: result,
+  });
+});
 
 export const TourControler = {
   createTour,
@@ -96,4 +108,5 @@ export const TourControler = {
   deleteTour,
   getAllTourTypes,
   createTourType,
+  updateTourType,
 };
